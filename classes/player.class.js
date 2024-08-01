598bc157abc5f47bc95681e29bec78589f44ea5c
Monomaze <brian@wester-mail.de>
2024-08-01 22:43:56 +0200

    added animations and movement based on input

--- classes/player.class.js
+++ classes/player.class.js
@@ -11,22 +11,58 @@ class Player extends MovableObject {
         'img/player_character/idle/player_idle5.png',
         'img/player_character/idle/player_idle6.png'
     ];
+    IMAGES_WALK = [
+        'img/player_character/walk/player_walk1.png',
+        'img/player_character/walk/player_walk2.png',
+        'img/player_character/walk/player_walk3.png',
+        'img/player_character/walk/player_walk4.png',
+        'img/player_character/walk/player_walk5.png',
+        'img/player_character/walk/player_walk6.png',
+        'img/player_character/walk/player_walk7.png',
+        'img/player_character/walk/player_walk8.png'
+    ];
     world;
+    speed = 3;
 
     constructor() {
-        super().loadImage('../img/idle/player_character/player_idle1.png');
-        this.loadImages(this.IMAGES_IDLE)
+        super().loadImage('../img/player_character/idle/player_idle1.png');
+        // this.loadImages(this.IMAGES_WALK);
 
         this.animate();
     }
 
     animate() {
         setInterval(() => {
-            let index = this.currentImage % this.IMAGES_IDLE.length;
-            let path = this.IMAGES_IDLE[index];
-            this.img = this.imageCache[path];
-            this.currentImage++;
-        }, 200)
+            if (this.world.keyboard.RIGHT) {
+                this.x += this.speed;
+                this.otherDirection = false;
+            }
+
+            if (this.world.keyboard.LEFT) {
+                this.x -= this.speed;
+                this.otherDirection = true;
+            }
+
+
+        }, 1000 / 60);
+
+        setInterval(() => {
+            if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
+                this.loadImages(this.IMAGES_WALK);
+                this.width = 80 * 1.5;
+                let index = this.currentImage % this.IMAGES_WALK.length;
+                let path = this.IMAGES_WALK[index];
+                this.img = this.imageCache[path];
+                this.currentImage++;
+            } else {
+                this.loadImages(this.IMAGES_IDLE);
+                this.width = 64 * 1.5;
+                let index = this.currentImage % this.IMAGES_IDLE.length;
+                let path = this.IMAGES_IDLE[index];
+                this.img = this.imageCache[path];
+                this.currentImage++;
+            }
+        }, 150);
 
     }
 
