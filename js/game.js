2809291b9302f52f6feba4a61fda3bd4061535a8
Monomaze <brian@wester-mail.de>
2024-08-01 21:17:58 +0200

    added EventListener for keydown/keyup events

--- js/game.js
+++ js/game.js
@@ -1,10 +1,47 @@
 let canvas;
 let ctx;
 let world;
+let keyboard = new Keyboard();
 
 function init() {
     canvas = document.getElementById('canvas');
-    world = new World(canvas);
+    world = new World(canvas, keyboard);
 
     console.log('My character is ', world.player);
 }
+
+window.addEventListener("keydown", (event) => {
+    console.log(event);
+    if (event.key === ' ') {
+        Keyboard.SPACE = true;
+        console.log('SPACE IS TRUE: ', Keyboard.SPACE);
+    }
+
+    if (event.key === 'ArrowRight') {
+        Keyboard.RIGHT = true;
+        console.log('RIGHT IS TRUE: ', Keyboard.RIGHT);
+    }
+
+    if (event.key === 'ArrowLeft') {
+        Keyboard.LEFT = true;
+        console.log('LEFT IS TRUE: ', Keyboard.RIGHT);
+    }
+});
+
+window.addEventListener("keyup", (event) => {
+    console.log(event);
+    if (event.key === ' ') {
+        Keyboard.SPACE = false;
+        console.log('SPACE IS FALSE: ', Keyboard.SPACE);
+    }
+
+    if (event.key === 'ArrowRight') {
+        Keyboard.RIGHT = false;
+        console.log('RIGHT IS FALSE: ', Keyboard.RIGHT);
+    }
+
+    if (event.key === 'ArrowLeft') {
+        Keyboard.LEFT = false;
+        console.log('LEFT IS FALSE: ', Keyboard.RIGHT);
+    }
+});
