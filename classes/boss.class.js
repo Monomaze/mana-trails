58cd856950580fde814af43daae5fa72a0e19702
Monomaze <brian@wester-mail.de>
2024-08-03 12:05:32 +0200

    added animation to boss monster and placed it in the world

--- classes/boss.class.js
+++ classes/boss.class.js
@@ -1,23 +1,26 @@
 class Boss extends MovableObject {
-    sizeMultiplier = 5;
-    width = 48 * this.sizeMultiplier;
-    height = 48 * this.sizeMultiplier;
-
+    sizeMultiplier = 4;
+    width = 64 * this.sizeMultiplier;
+    height = 64 * this.sizeMultiplier;
+    y = 480 - this.height - 70; 
     IMAGES_WALK = [
-       ' img/enemies/goblin/goblin_walk1.png',
-        'img/enemies/goblin/goblin_walk2.png',
-        'img/enemies/goblin/goblin_walk3.png',
-        'img/enemies/goblin/goblin_walk4.png',
-        'img/enemies/goblin/goblin_walk5.png',
-        'img/enemies/goblin/goblin_walk6.png',
-        'img/enemies/goblin/goblin_walk7.png',
-        'img/enemies/goblin/goblin_walk8.png'
+        'img/enemies/skeleton/skeleton_walk1.png',
+        'img/enemies/skeleton/skeleton_walk2.png',
+        'img/enemies/skeleton/skeleton_walk3.png',
+        'img/enemies/skeleton/skeleton_walk4.png'
     ];
 
     constructor() {
         super().loadImage(this.IMAGES_WALK[0]);
         this.loadImages(this.IMAGES_WALK);
-        this.x = 700;
+        this.x = 2200;
+        this.animate();
+    }
+
+    animate() {
+        setInterval(() => {
+            this.playAnimation(this.IMAGES_WALK);
+        }, 150)
     }
 
 }
