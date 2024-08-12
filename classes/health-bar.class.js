2ec0e4d9666dce9416c9de1a88563e24aa8517b4
Monomaze <brian@wester-mail.de>
2024-08-12 15:26:50 +0200

    corrected typo in folder name and paths

--- classes/health-bar.class.js
+++ classes/health-bar.class.js
@@ -1,17 +1,17 @@
 class HealthBar extends StatusBar {
     percentage = 100;
     IMAGES = [
-        'img/UI/heatlh_bar/health_bar00.png',
-        'img/UI/heatlh_bar/health_bar10.png',
-        'img/UI/heatlh_bar/health_bar20.png',
-        'img/UI/heatlh_bar/health_bar30.png',
-        'img/UI/heatlh_bar/health_bar40.png',
-        'img/UI/heatlh_bar/health_bar50.png',
-        'img/UI/heatlh_bar/health_bar60.png',
-        'img/UI/heatlh_bar/health_bar70.png',
-        'img/UI/heatlh_bar/health_bar80.png',
-        'img/UI/heatlh_bar/health_bar90.png',
-        'img/UI/heatlh_bar/health_bar100.png'
+        'img/UI/health_bar/health_bar00.png',
+        'img/UI/health_bar/health_bar10.png',
+        'img/UI/health_bar/health_bar20.png',
+        'img/UI/health_bar/health_bar30.png',
+        'img/UI/health_bar/health_bar40.png',
+        'img/UI/health_bar/health_bar50.png',
+        'img/UI/health_bar/health_bar60.png',
+        'img/UI/health_bar/health_bar70.png',
+        'img/UI/health_bar/health_bar80.png',
+        'img/UI/health_bar/health_bar90.png',
+        'img/UI/health_bar/health_bar100.png'
     ];
 
     constructor() {
