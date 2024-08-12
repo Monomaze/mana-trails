4eba7982d61b7df9974fcb142be1eece43aeae15
Monomaze <brian@wester-mail.de>
2024-08-12 22:50:15 +0200

    fixed issure where background was moving when pressing left and right at the same time

--- classes/parallax-background.class.js
+++ classes/parallax-background.class.js
@@ -17,22 +17,24 @@ class ParallaxBackground extends MovableObject {
     animate() {
         setInterval(() => {
             if (world !== undefined) {
-                if (this.world.keyboard.RIGHT && this.world.player.x < this.world.level.levelEndXRight && !this.world.player.isDead()) {
-                    if (this.direction === 'right') {
-                        this.moveRight();
-                    } else if (this.direction === 'left') {
-                        this.moveLeft();
+                if (!(this.world.keyboard.RIGHT || this.world.keyboard.LEFT)) {
+                    if (this.world.keyboard.RIGHT && this.world.player.x < this.world.level.levelEndXRight && !this.world.player.isDead()) {
+                        if (this.direction === 'right') {
+                            this.moveRight();
+                        } else if (this.direction === 'left') {
+                            this.moveLeft();
+                        }
+                        this.otherDirection = false;
                     }
-                    this.otherDirection = false;
-                }
-
-                if (this.world.keyboard.LEFT && this.world.player.x > this.world.level.levelEndXLeft && !this.world.player.isDead()) {
-                    if (this.direction === 'right') {
-                        this.moveLeft();
-                    } else if (this.direcition === 'left') {
-                        this.moveRight();
+    
+                    if (this.world.keyboard.LEFT && this.world.player.x > this.world.level.levelEndXLeft && !this.world.player.isDead()) {
+                        if (this.direction === 'right') {
+                            this.moveLeft();
+                        } else if (this.direcition === 'left') {
+                            this.moveRight();
+                        }
+                        this.otherDirection = true;
                     }
-                    this.otherDirection = true;
                 }
             }
         }, 1000 / 60);
