e97d450ef1a66e5d067bc9bd7e83b1cd2e3cd361
Monomaze <brian@wester-mail.de>
2024-08-14 15:02:15 +0200

    added new class for collectable object: mana pots

---
+++ classes/mana-pot.class.js
@@ -0,0 +1,18 @@
+class ManaPot extends CollectableObject {
+    IMAGES_SPIN = [
+        'img/items/mana_pot/mana_pot01.png',
+        'img/items/mana_pot/mana_pot02.png',
+        'img/items/mana_pot/mana_pot03.png',
+        'img/items/mana_pot/mana_pot04.png',
+        'img/items/mana_pot/mana_pot05.png',
+        'img/items/mana_pot/mana_pot06.png',
+        'img/items/mana_pot/mana_pot07.png',
+        'img/items/mana_pot/mana_pot08.png'
+    ];
+
+    constructor() {
+        super().loadImage('img/items/mana_pot/mana_pot01.png');
+        this.loadImages(this.IMAGES_SPIN);
+        this.animate();
+    }
+}
