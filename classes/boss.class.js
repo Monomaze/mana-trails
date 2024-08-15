55494ce613e14a0e6f08252df77f3698b3fc0a33
Monomaze <brian@wester-mail.de>
2024-08-15 13:28:21 +0200

    added img arrays to clas

--- classes/boss.class.js
+++ classes/boss.class.js
@@ -1,14 +1,44 @@
 class Boss extends Enemies {
-    sizeMultiplier = 4;
+    sizeMultiplier = 3;
     width = 64 * this.sizeMultiplier;
     height = 64 * this.sizeMultiplier;
     y = 410 - this.height;
     health = 100;
+    speed = 1;
     IMAGES_WALK = [
-        'img/enemies/skeleton/skeleton_walk1.png',
-        'img/enemies/skeleton/skeleton_walk2.png',
-        'img/enemies/skeleton/skeleton_walk3.png',
-        'img/enemies/skeleton/skeleton_walk4.png'
+        'img/enemies/boss/boss_walk01.png',
+        'img/enemies/boss/boss_walk02.png',
+        'img/enemies/boss/boss_walk03.png',
+        'img/enemies/boss/boss_walk04.png',
+        'img/enemies/boss/boss_walk05.png',
+        'img/enemies/boss/boss_walk06.png',
+        'img/enemies/boss/boss_walk07.png',
+        'img/enemies/boss/boss_walk08.png'
+    ];
+    IMAGES_ATTACK = [
+        'img/enemies/boss/boss_attack01.png',
+        'img/enemies/boss/boss_attack02.png',
+        'img/enemies/boss/boss_attack03.png',
+        'img/enemies/boss/boss_attack04.png',
+        'img/enemies/boss/boss_attack05.png',
+        'img/enemies/boss/boss_attack06.png',
+        'img/enemies/boss/boss_attack07.png',
+        'img/enemies/boss/boss_attack08.png'
+    ];
+    IMAGES_DEATH = [
+        'img/enemies/boss/boss_death01.png',
+        'img/enemies/boss/boss_death02.png',
+        'img/enemies/boss/boss_death03.png',
+        'img/enemies/boss/boss_death04.png',
+        'img/enemies/boss/boss_death05.png',
+        'img/enemies/boss/boss_death06.png',
+        'img/enemies/boss/boss_death07.png',
+        'img/enemies/boss/boss_death08.png',
+        'img/enemies/boss/boss_death09.png',
+        'img/enemies/boss/boss_death10.png',
+        'img/enemies/boss/boss_death11.png',
+        'img/enemies/boss/boss_death12.png',
+        'img/enemies/boss/boss_death13.png'
     ];
 
     constructor() {
@@ -23,5 +53,4 @@ class Boss extends Enemies {
             this.playAnimation(this.IMAGES_WALK);
         }, 150)
     }
-
 }
