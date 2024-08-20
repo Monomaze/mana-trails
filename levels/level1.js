6848b0ba1275c0773c63266ecc97e145e42f8227
Monomaze <brian@wester-mail.de>
2024-08-20 20:42:39 +0200

    changed level1 to get created by a function

--- levels/level1.js
+++ levels/level1.js
@@ -1,38 +1,42 @@
-const level1 = new Level(
-    [
-        new Mushroom(),
+let level1;
 
-    ],
-    [
-        new ParallaxBackground('img/background/background_layer_1.png', -719, 'left'),
-        new ParallaxBackground('img/background/background_layer_1.png', 0, 'left'),
-        new ParallaxBackground('img/background/background_layer_1.png', 719, 'left'),
-        new ParallaxBackground('img/background/background_layer_1.png', 719 * 2, 'left'),
-        new ParallaxBackground('img/background/background_layer_1.png', 719 * 3, 'left'),
+function createLevel() {
+    level1 = new Level(
+        [
+            new Mushroom(),
+        ],
+        [
+            new ParallaxBackground('img/background/background_layer_1.png', -719, 'left'),
+            new ParallaxBackground('img/background/background_layer_1.png', 0, 'left'),
+            new ParallaxBackground('img/background/background_layer_1.png', 719, 'left'),
+            new ParallaxBackground('img/background/background_layer_1.png', 719 * 2, 'left'),
+            new ParallaxBackground('img/background/background_layer_1.png', 719 * 3, 'left'),
+    
+            new ParallaxBackground('img/background/background_layer_2.png', -719, 'right'),
+            new ParallaxBackground('img/background/background_layer_2.png', 0, 'right'),
+            new ParallaxBackground('img/background/background_layer_2.png', 719, 'right'),
+            new ParallaxBackground('img/background/background_layer_2.png', 719 * 2, 'right'),
+            new ParallaxBackground('img/background/background_layer_2.png', 719 * 3, 'right'),
+    
+            new ParallaxBackground('img/background/background_layer_3.png', -719, 'left'),
+            new ParallaxBackground('img/background/background_layer_3.png', 0, 'left'),
+            new ParallaxBackground('img/background/background_layer_3.png', 719, 'left'),
+            new ParallaxBackground('img/background/background_layer_3.png', 719 * 2, 'left'),
+            new ParallaxBackground('img/background/background_layer_3.png', 719 * 3, 'left')
+        ], 
+        [
+            new BackgroundObject('img/background/ground.png', -719),
+            new BackgroundObject('img/background/ground.png', 0),
+            new BackgroundObject('img/background/ground.png', 719),
+            new BackgroundObject('img/background/ground.png', 719 * 2),
+            new BackgroundObject('img/background/ground.png', 719 * 3)
+        ],
+        [
+            new ManaPot(),
+            new Scroll(150),
+            new Scroll(250),
+            new Scroll(350)
+        ]
+    );
+}
 
-        new ParallaxBackground('img/background/background_layer_2.png', -719, 'right'),
-        new ParallaxBackground('img/background/background_layer_2.png', 0, 'right'),
-        new ParallaxBackground('img/background/background_layer_2.png', 719, 'right'),
-        new ParallaxBackground('img/background/background_layer_2.png', 719 * 2, 'right'),
-        new ParallaxBackground('img/background/background_layer_2.png', 719 * 3, 'right'),
-
-        new ParallaxBackground('img/background/background_layer_3.png', -719, 'left'),
-        new ParallaxBackground('img/background/background_layer_3.png', 0, 'left'),
-        new ParallaxBackground('img/background/background_layer_3.png', 719, 'left'),
-        new ParallaxBackground('img/background/background_layer_3.png', 719 * 2, 'left'),
-        new ParallaxBackground('img/background/background_layer_3.png', 719 * 3, 'left')
-    ], 
-    [
-        new BackgroundObject('img/background/ground.png', -719),
-        new BackgroundObject('img/background/ground.png', 0),
-        new BackgroundObject('img/background/ground.png', 719),
-        new BackgroundObject('img/background/ground.png', 719 * 2),
-        new BackgroundObject('img/background/ground.png', 719 * 3)
-    ],
-    [
-        new ManaPot(),
-        new Scroll(150),
-        new Scroll(250),
-        new Scroll(350)
-    ]
-);
